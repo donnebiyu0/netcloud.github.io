28e74e62e1253eca11b9362a2a498eb15fc510c3
donnebiyu0 <nebiyuab00@gmail.com>
2023-10-11 11:19:05 -0700

    folder structure setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+import useStyles from "./components/styles";
+import {
+  Actors,
+  MovieInformation,
+  Profile,
+  NavBar,
+  Movies,
+} from "./components";
 
 function App() {
-  return <div className="App">film</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.Container}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+          <Route exact path="/movies/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieInformation = () => {
+  return (
+    <div>
+      <h1>Movie component</h1>
+    </div>
+  )
+}
+
+export default MovieInformation
